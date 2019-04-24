b7c0ccfbab1eaf6a1eccff1a9eec729900c78a98
Kate Efimova <kate.efimova0@gmail.com>
2019-04-23 18:46:37 -0700

    Add files via upload

---
+++ Lab 1/utilPackage/FileIO.java
@@ -0,0 +1,17 @@
+package utilPackage;
+import java.io.Serializable;
+import modelPackage.Automotive;
+
+public class FileIO implements Serializable {
+	public Automotive readFile(String fileName) {
+		return null;
+	}
+	
+	public void serialize(Automotive car, String fileName) {
+		
+	}
+	
+	public Automotive deserialize(String fileName) {
+		return null;
+	}
+}
