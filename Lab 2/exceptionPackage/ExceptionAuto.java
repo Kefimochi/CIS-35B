f45caffbdedb43593c31dfcaa4c36ad1e45e222a
Kate Efimova <kate.efimova0@gmail.com>
2019-05-08 13:25:22 -0700

    Add files via upload

--- Lab 2/exceptionPackage/ExceptionAuto.java
+++ Lab 2/exceptionPackage/ExceptionAuto.java
@@ -1,5 +1,5 @@
 package exceptionPackage;
-
+import exceptionPackage.*;
 import java.io.PrintWriter;
 
 public class ExceptionAuto extends Exception {
@@ -17,38 +17,73 @@ public class ExceptionAuto extends Exception {
 		fix(errorNum);
 	}
 	
+	public int getErrorNum() {
+		return errorNum;
+	}
+	
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+	
+	public void setErrorNum(int num) {
+		errorNum = num;
+	}
+	
+	public void setErrorMsg(String msg) {
+		errorMsg = msg;
+	}
+	
 	public String fix(int num) {
-		ExceptionFixes t = new ExceptionFixes(num);
-		return t.fixError(num);
+		ExceptionFixes x = new ExceptionFixes();
+		switch (num) { 
+	        case 101: 
+	            x.fix101(101);
+	        case 201: 
+	        	errorMsg = "Missing base price"; 
+	            break; 
+	        case 301: 
+	        	errorMsg = "Missing array size"; 
+	            break; 
+	        case 401: 
+	        	errorMsg = "Missing option set data"; 
+	            break; 
+	        case 501: 
+	        	errorMsg = "Missing option data"; 
+	            break; 
+	        default: 
+	        	errorMsg = "Some unknown error occured!"; 
+	            break; 
+		} 
+		return errorMsg;
 	}
 	
 	public void matchError(int num) {
 		switch (num) { 
-        case 101: 
-            errorMsg = "Missing file name"; 
-            break; 
-        case 201: 
-        	errorMsg = "Missing base price"; 
-            break; 
-        case 301: 
-        	errorMsg = "Missing array size"; 
-            break; 
-        case 401: 
-        	errorMsg = "Missing option set data"; 
-            break; 
-        case 501: 
-        	errorMsg = "Missing option data"; 
-            break; 
-        default: 
-        	errorMsg = "Some unknown error occured!"; 
-            break; 
+	        case 101: 
+	            errorMsg = "Missing file name"; 
+	            break; 
+	        case 201: 
+	        	errorMsg = "Missing base price"; 
+	            break; 
+	        case 301: 
+	        	errorMsg = "Missing array size"; 
+	            break; 
+	        case 401: 
+	        	errorMsg = "Missing option set data"; 
+	            break; 
+	        case 501: 
+	        	errorMsg = "Missing option data"; 
+	            break; 
+	        default: 
+	        	errorMsg = "Some unknown error occured!"; 
+	            break; 
         } 
 	}
 	
 	public void log(int num) {
 		System.out.printf("\n~~~~~~~Error number %d: %s\n", errorNum, errorMsg);
-		PrintWriter writer = new PrintWriter("D:\\Coding Projects\\CIS 35B\\Lab 1\\data\\exception-log.txt", "UTF-8");
-		writer.println("\n~~~~~~~Error number %d: %s\n", errorNum, errorMsg);
-		writer.close();
+//		PrintWriter writer = new PrintWriter("D:\\Coding Projects\\CIS 35B\\Lab 1\\data\\exception-log.txt", "UTF-8");
+//		writer.printf("\n~~~~~~~Error number %d: %s\n", errorNum, errorMsg);
+//		writer.close();
 	}
 }
--- Lab 2/exceptionPackage/ExceptionFixes.java
+++ Lab 2/exceptionPackage/ExceptionFixes.java
@@ -6,50 +6,57 @@ import adapterPackage.BuildAuto;
 import adapterPackage.CreatableAuto;
 
 public class ExceptionFixes extends Exception {
-	private int num;
-	
-	protected ExceptionFixes() {
-		num = -1;
-		fixError(num);
+//	protected String fixError(int num) {
+//		switch (num) { 
+//        case 101: 
+////        	errorMsg = "Missing file name"; 
+//        	System.out.printf("Please enter new file location: ");
+//        	
+//        	Scanner ss = new Scanner(System.in);
+//        	String s = ss.next(); // Reads in first word
+//        	
+//        	CreatableAuto a1 = new BuildAuto();
+//        	a1.BuildAuto(s);
+//        	break; 
+//        case 201: 
+////        	errorMsg = "Missing base price"; 
+//        	System.out.printf("Please enter new base price: ");
+//        	Scanner aa = new Scanner(System.in);
+//        	int a = Integer.parseInt(aa.next());
+//        	BuildAuto q = new BuildAuto();
+//            break; 
+//        case 301: 
+////        	errorMsg = "Missing array size"; 
+//            break; 
+//        case 401: 
+////        	errorMsg = "Missing option set data"; 
+//            break; 
+//        case 501: 
+////        	errorMsg = "Missing option data"; 
+//            break; 
+//        default: 
+////        	errorMsg = "Some unknown error occured!"; 
+//            break; 
+//        }
+//		return "";
+//	}
+	public void fix101(int errorNum) {
+		
 	}
 	
-	protected ExceptionFixes(int num) {
-		this.num = num;
-		fixError(num);
+	public void fix201(int errorNum) {
+		
 	}
 	
-	protected String fixError(int num) {
-		switch (num) { 
-        case 101: 
-//        	errorMsg = "Missing file name"; 
-        	System.out.printf("Please enter new file location: ");
-        	
-        	Scanner ss = new Scanner(System.in);
-        	String s = ss.next(); // Reads in first word
-        	
-        	CreatableAuto a1 = new BuildAuto();
-        	a1.BuildAuto(s);
-        	break; 
-        case 201: 
-//        	errorMsg = "Missing base price"; 
-        	System.out.printf("Please enter new base price: ");
-        	Scanner aa = new Scanner(System.in);
-        	int a = Integer.parseInt(aa.next());
-        	BuildAuto q = new BuildAuto();
-            break; 
-        case 301: 
-//        	errorMsg = "Missing array size"; 
-            break; 
-        case 401: 
-//        	errorMsg = "Missing option set data"; 
-            break; 
-        case 501: 
-//        	errorMsg = "Missing option data"; 
-            break; 
-        default: 
-//        	errorMsg = "Some unknown error occured!"; 
-            break; 
-        }
-		return "";
+	public void fix301(int errorNum) {
+		
+	}
+	
+	public void fix401(int errorNum) {
+		
+	}
+
+	public void fix501(int errorNum) {
+	
 	}
 }
