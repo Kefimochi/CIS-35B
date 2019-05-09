4d09634991908ac92e5f4e493494682ebebd1b51
Kate Efimova <kate.efimova0@gmail.com>
2019-05-08 22:36:45 -0700

    Add files via upload

--- Lab 2/exceptionPackage/ExceptionAuto.java
+++ Lab 2/exceptionPackage/ExceptionAuto.java
@@ -7,14 +7,32 @@ public class ExceptionAuto extends Exception {
 	private String errorMsg;
 	
 	public ExceptionAuto() {
-		errorNum = -1;
-		matchError(errorNum);
+		this(-1);
 	}
 	
 	public ExceptionAuto(int errorNum) {
 		this.errorNum = errorNum;
-		matchError(errorNum);
-		fix(errorNum);
+		switch (errorNum) { 
+	        case 101: 
+	            errorMsg = "Missing file name"; 
+	            break; 
+	        case 201: 
+	        	errorMsg = "Missing base price"; 
+	            break; 
+	        case 301: 
+	        	errorMsg = "Missing option set array size"; 
+	            break; 
+	        case 401: 
+	        	errorMsg = "Missing option set name"; 
+	            break; 
+	        case 501: 
+	        	errorMsg = "Missing option name"; 
+	            break; 
+	        case -1: 
+	        	errorMsg = "Some unknown error occured!"; 
+	            break; 
+		} 
+		log();
 	}
 	
 	public int getErrorNum() {
@@ -33,55 +51,13 @@ public class ExceptionAuto extends Exception {
 		errorMsg = msg;
 	}
 	
-	public String fix(int num) {
+	public String fix() {
 		ExceptionFixes x = new ExceptionFixes();
-		switch (num) { 
-	        case 101: 
-	            x.fix101(101);
-	        case 201: 
-	        	errorMsg = "Missing base price"; 
-	            break; 
-	        case 301: 
-	        	errorMsg = "Missing array size"; 
-	            break; 
-	        case 401: 
-	        	errorMsg = "Missing option set data"; 
-	            break; 
-	        case 501: 
-	        	errorMsg = "Missing option data"; 
-	            break; 
-	        default: 
-	        	errorMsg = "Some unknown error occured!"; 
-	            break; 
-		} 
-		return errorMsg;
+		return x.fix();
 	}
 	
-	public void matchError(int num) {
-		switch (num) { 
-	        case 101: 
-	            errorMsg = "Missing file name"; 
-	            break; 
-	        case 201: 
-	        	errorMsg = "Missing base price"; 
-	            break; 
-	        case 301: 
-	        	errorMsg = "Missing array size"; 
-	            break; 
-	        case 401: 
-	        	errorMsg = "Missing option set data"; 
-	            break; 
-	        case 501: 
-	        	errorMsg = "Missing option data"; 
-	            break; 
-	        default: 
-	        	errorMsg = "Some unknown error occured!"; 
-	            break; 
-        } 
-	}
-	
-	public void log(int num) {
-		System.out.printf("\n~~~~~~~Error number %d: %s\n", errorNum, errorMsg);
+	public void log() {
+		System.out.printf("\n~~~~~~~Error %d: %s\n", errorNum, errorMsg);
 //		PrintWriter writer = new PrintWriter("D:\\Coding Projects\\CIS 35B\\Lab 1\\data\\exception-log.txt", "UTF-8");
 //		writer.printf("\n~~~~~~~Error number %d: %s\n", errorNum, errorMsg);
 //		writer.close();
--- Lab 2/exceptionPackage/ExceptionFixes.java
+++ Lab 2/exceptionPackage/ExceptionFixes.java
@@ -1,62 +1,14 @@
 package exceptionPackage;
-
 import java.util.Scanner;
 
 import adapterPackage.BuildAuto;
 import adapterPackage.CreatableAuto;
 
-public class ExceptionFixes extends Exception {
-//	protected String fixError(int num) {
-//		switch (num) { 
-//        case 101: 
-////        	errorMsg = "Missing file name"; 
-//        	System.out.printf("Please enter new file location: ");
-//        	
-//        	Scanner ss = new Scanner(System.in);
-//        	String s = ss.next(); // Reads in first word
-//        	
-//        	CreatableAuto a1 = new BuildAuto();
-//        	a1.BuildAuto(s);
-//        	break; 
-//        case 201: 
-////        	errorMsg = "Missing base price"; 
-//        	System.out.printf("Please enter new base price: ");
-//        	Scanner aa = new Scanner(System.in);
-//        	int a = Integer.parseInt(aa.next());
-//        	BuildAuto q = new BuildAuto();
-//            break; 
-//        case 301: 
-////        	errorMsg = "Missing array size"; 
-//            break; 
-//        case 401: 
-////        	errorMsg = "Missing option set data"; 
-//            break; 
-//        case 501: 
-////        	errorMsg = "Missing option data"; 
-//            break; 
-//        default: 
-////        	errorMsg = "Some unknown error occured!"; 
-//            break; 
-//        }
-//		return "";
-//	}
-	public void fix101(int errorNum) {
-		
-	}
-	
-	public void fix201(int errorNum) {
-		
-	}
-	
-	public void fix301(int errorNum) {
-		
-	}
-	
-	public void fix401(int errorNum) {
-		
-	}
-
-	public void fix501(int errorNum) {
-	
+public class ExceptionFixes extends Exception { 
+	public String fix() {
+		System.out.printf("An error occured, please re-enter your information below:\n");
+		Scanner ss = new Scanner(System.in);
+    	String s = ss.nextLine();
+		return s;
 	}
 }
